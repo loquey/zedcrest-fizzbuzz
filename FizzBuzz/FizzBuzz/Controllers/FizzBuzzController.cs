6e0539558910d4bb6429e8010e70aabe883ec9e1
Titilope Asare <titilopeasare@gmail.com>
2020-10-03 13:27:12 +0100

    Initial project setup with working endpoint that echo request data

---
+++ FizzBuzz/FizzBuzz/Controllers/FizzBuzzController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Routing.Template;
+
+namespace FizzBuzz.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FizzBuzzController : ControllerBase
+    {
+        public FizzBuzzController()
+        {
+                
+        }
+
+        [Route("{number:int}")]
+        public IActionResult Get(int number)
+        {
+            return Ok($"fizzbuzz {number}");
+        }
+    }
+}
